1df191ce76f1d47eb9294aa5865b02d73615ebea
Kelvin Porto <kurswail67@gmail.com>
2023-11-09 08:23:42 -0300

    09/11

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        Pessoa qualquer = new Pessoa();
+        qualquer.setNome ("Rebeca Andrade");
+        System.out.println(qualquer.getNome());
+
+        Esportista jogadora = new Esportista();
+        Esportista surfista = new Esportista(345);
+        jogadora.setNome("Taissa do Volei");
+        System.out.println(jogadora.getNome());
+
+        // Polimorfismo
+        Pessoa outra = new Pessoa();
+        String retorno = outra.sacar();
+        System.out.println(retorno);
+        outra = new Esportista();
+        retorno = outra.sacar();
+        System.out.println(retorno);
+
+
+
+
+
+    }
+}
